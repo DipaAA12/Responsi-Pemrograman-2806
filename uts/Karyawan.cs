8d30fa5a200a21f669bca23331e061a58f63f449
dipaalmadima12@gmail.com <serafinalisu@gmail.com>
2020-04-15 12:53:54 +0700

    Responsi

---
+++ uts/Karyawan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts
+{
+    public class Karyawan
+    {
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int No { get; set; }
+        public int NIM { get; set; }
+        public Karyawan(string Nama, int GajiBulanan, int No, int NIK)
+        {
+            this.Nama = Nama;
+            this.No = No;
+            this.NIM = NIM;
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+        }
+        public void karyawan()
+        {
+            Console.WriteLine("{0}  \t {1} \t {2} \t {3} ", No, NIM, Nama, GajiBulanan);
+        }
+        public void GajiTambahan()
+        {
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+            Console.WriteLine("{0}  \t {1} \t {2} \t {3} ", No, NIM, Nama, GajiBulanan*0.1+GajiBulanan);
+        }
+    }
+}
---
+++ uts/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karyawan karyawan1 = new Karyawan();
+            karyawan1.No = 1;
+            karyawan1.NIM = 19112806;
+            karyawan1.Nama = "Henita";
+            karyawan1.GajiBulanan = 3000000;
+
+            Karyawan karyawan2 = new Karyawan();
+            karyawan2.No = 2;
+            karyawan2.NIM = 19112847;
+            karyawan2.Nama = "Lanang";
+            karyawan2.GajiBulanan = 2000000;
+
+            Console.WriteLine("No /   NIM/Nama/Gaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.karyawan();
+            karyawan2.karyawan();
+            Console.WriteLine("");
+            Console.WriteLine("Anda dapat tambahan gaji 10%");
+            Console.WriteLine("");
+            Console.WriteLine("No /   NIM/Nama/Gaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.GajiTambahan();
+            karyawan2.GajiTambahan();
+
+            Console.ReadKey();
+        }
+    }
+}
